2e828db42ccb0e9950e03f902a91fd8e41db8e44
Vo Cong Danh <Danh.vo2012@hcmut.edu.vn>
2022-11-05 21:05:42 +0700

    Midterm-finalize

---
+++ Core/Inc/dis7SEG.h
@@ -0,0 +1,14 @@
+/*
+ * dis7SEG.h
+ *
+ *  Created on: Nov 5, 2022
+ *      Author: ASUS
+ */
+
+#ifndef INC_DIS7SEG_H_
+#define INC_DIS7SEG_H_
+#include "main.h"
+
+void display7SEG(int num);
+
+#endif /* INC_DIS7SEG_H_ */
---
+++ Core/Src/global.c
@@ -0,0 +1,12 @@
+/*
+ * global.c
+ *
+ *  Created on: Nov 5, 2022
+ *      Author: ASUS
+ */
+#include "global.h"
+
+int counter = 0;
+int mode =0;
+
+
